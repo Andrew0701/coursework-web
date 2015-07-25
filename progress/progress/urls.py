e18a505b9ad612e366c62011ad6bd1a0915433e8
Andrew Uljanenko <andrew.uljanenko@gmail.com>
2015-07-25 19:14:42 +0300

    Локализация. Добавлены английский и украинский языки. #5

--- progress/progress/urls.py
+++ progress/progress/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('home.urls',namespace='home'))
+    url(r'^', include('home.urls',namespace='home')),
+    url(r'^i18n/', include('django.conf.urls.i18n'))
 ]
