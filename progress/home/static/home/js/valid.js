6ccd6fb654b238a5f1b87b42c58c41faf9dc002a
M1xey <m.1.xey@ya.ru>
2015-08-21 22:19:05 +0300

    Готова кнопочка "Зарегистрировать" :)

--- progress/home/static/home/js/valid.js
+++ progress/home/static/home/js/valid.js
@@ -1,4 +1,4 @@
-﻿function delMessageAfter(element) {
+function delMessageAfter(element) {
 
 	nextElementClass = element.next().attr('class')
 
@@ -23,11 +23,15 @@ function validate(element,pattern,errorMessage){
 	text = element.val()
 	if (pattern.test(text)) {
 		successMessageAfter(element)
+        all_checking[element.attr('name')] = true;
+        block_submit_student();
 		return true;
 	}
 	else {
-		errorMessageAfter(element,errorMessage)
-		return false;
+		errorMessageAfter(element,errorMessage);
+        all_checking[element.attr('name')] = false;
+		block_submit_student();
+        return false;
 	}
 }
 
@@ -50,4 +54,35 @@ function validatePassword(element) {
 function validatePasswordConfirmation(element,id) {
 	firstPassword = $(`input[name=password][id=${id}`).val();
 	return validate(element,RegExp('^'+firstPassword+'$'),'Пароли не совпадают')
+}
+
+var all_checking = {};
+
+function block_submit_student(){
+    
+    var count_right_values = 0;
+    var need_count_right = 7;   //Необходимое количество правильно заполненнных полей
+    for (var val in all_checking){
+        if (all_checking[val]) {
+            count_right_values++;
+        }
+    }
+    if (count_right_values >= need_count_right){ 
+        $('input[type=submit]').prop('disabled',false);
+    }else{
+        $('input[type=submit]').prop('disabled',true);
+    }
+}
+
+function listener_on_change(){
+    $('#reg_student').prop('disabled',true);
+    $('#reg_student').mouseover(give_the_massiv);
+    
+}
+
+function give_the_massiv(){
+    for (var val in all_checking){
+        console.log(val);
+    }
+    console.log(all_checking);
 }
